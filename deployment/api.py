b286cc74929b2805a8ac035d38b71124293fca83
Ed Longbottom <eddlong94@gmail.com>
2021-12-11 16:25:31 +0000

    Added files for an automated deployment workflow

--- deployment/api.py
+++ deployment/api.py
@@ -8,12 +8,19 @@ This module defines a simple REST API for a Machine Learning (ML) model
 
 from flask import Flask, jsonify, request, make_response
 import numpy as np
+from yaml import load, Loader
 import pickle
 
-app = Flask(__name__)
-model = pickle.load(open('dtc_model.sav','rb'))
+with open('config.yaml','r') as config_file:
+    config = load(config_file, Loader=Loader)
 
-@app.route('/predict', methods=['POST'])
+service_name = config['SERVICE_NAME']
+api_version = config['API_VERSION']
+
+app = Flask(__name__)
+model = pickle.load(open('ml_model.sav','rb'))
+
+@app.route(f'/{service_name}/v{api_version}/predict', methods=['POST'])
 def score():
     ''' Prediction web service'''
     try:
--- deployment/deployment.sh
+++
@@ -1,7 +0,0 @@
-
-
-# a different requirements.txt is used for model serving than for the project as a whole
-
-# docker build
-
-# helm install to kubernetes
