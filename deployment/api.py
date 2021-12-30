46c43fe2c3b6e8bfdfc923a39771ed5ed3e90111
Ed Longbottom <eddlong94@gmail.com>
2021-12-30 15:59:29 +0000

    Amended files for deployment fix

--- deployment/api.py
+++ deployment/api.py
@@ -7,9 +7,10 @@ This module defines a simple REST API for a Machine Learning (ML) model
 """
 
 from flask import Flask, jsonify, request, make_response
-import numpy as np
+import pandas as pd
 from yaml import load, Loader
-import pickle
+import joblib
+import glob
 
 with open('config.yaml','r') as config_file:
     config = load(config_file, Loader=Loader)
@@ -18,15 +19,16 @@ service_name = config['SERVICE_NAME']
 api_version = config['API_VERSION']
 
 app = Flask(__name__)
-model = pickle.load(open('ml_model.sav','rb'))
+
+model = joblib.load(glob.glob('*.pkl')[0])
 
 @app.route(f'/{service_name}/v{api_version}/predict', methods=['POST'])
 def score():
     ''' Prediction web service'''
     try:
-        X = request.json['X']
-        prediction = int(model.predict(np.array(X).reshape(1,-1)))
-        return make_response(jsonify({'prediction': prediction}))
+        X_inf = pd.DataFrame(request.json)
+        scores = model.predict(X_inf).tolist()
+        return make_response(jsonify({'predictions': scores}))
     except ValueError:
         raise RuntimeError('Error: Features are not in the correct format.')
 
