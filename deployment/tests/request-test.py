ffd2375773f987e2de5eb9e73f04f905d470c1a9
Ed Longbottom <eddlong94@gmail.com>
2022-02-04 18:04:04 +1100

    Amended web service test

--- deployment/tests/request-test.py
+++ deployment/tests/request-test.py
@@ -7,6 +7,7 @@ response = requests.post(url="http://127.0.0.1:5000/titanic/v0.0.1/predict",
                         data=json.dumps({'PassengerId':[892],'Pclass':[3],'Name':['Kelly, Mr. James'],'Sex':['male'],
                                         'Age':[34.5],'SibSp':[0],'Fare':[7.8292],'Embarked':['S']}))
 
+# run tests
 print(response.json()) 
 assert len(response.json()) == 1
 assert isinstance(response.json()['predictions'],list)
